eb693041e32a18d4f09f8b54ed7abb7a18bfe964
LAPTOP-JJBSSHEC\user <accordcu.gt201@gmail.com>
2025-05-28 15:34:38 +0900

    コミットしました2

---
+++ omikuji/click-number/js/scripts.js
@@ -0,0 +1,12 @@
+// This file is intentionally left blank.
+var num = 1; 
+var elm = document.createElement("button");
+elm.innerHTML = num;
+elm.setAttribute("id", num); 
+elm.setAttribute("class", "circle"); 
+elm.setAttribute("onclick", "remove()");
+document.getElementById("main").appendChild(elm);
+
+document.remove = function() {
+	document.getElementById("main").removeChild(document.getElementById("1"));
+}
